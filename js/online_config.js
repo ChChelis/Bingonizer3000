85867d4913e88381ef678f2728ee114ec1ac01df
Chelis <contato.chelles@gmail.com>
2026-05-11 12:30:30 -0300

    Enable Firebase online sync

--- js/online_config.js
+++ js/online_config.js
@@ -1,14 +1,15 @@
 window.BINGO_ONLINE_CONFIG = {
-  enabled: false,
+  enabled: true,
   provider: "firebase",
   sdk_version: "10.14.1",
   room_path: "rooms/sample_room_001",
   firebase_config: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDnqVliOuaps6gBw6OukZv2Ok3jBG8MJZ0",
+    authDomain: "bingonizer3000.firebaseapp.com",
+    projectId: "bingonizer3000",
+    storageBucket: "bingonizer3000.firebasestorage.app",
+    messagingSenderId: "916597691880",
+    appId: "1:916597691880:web:a418d4a5f7fd294782405d",
+    measurementId: "G-MJ0LPKGFG2"
   }
 };
